db3e80479562b99729ca4a8eca41cd35add0ffb6
TaamZID <tamjidalam00@gmail.com>
2022-10-11 21:24:34 +0600

    correct-answer-done

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -5,12 +5,16 @@ import QuizQuestions from "../QuizQuestions/QuizQuestions";
 const Quiz = () => {
   const { data } = useLoaderData();
   console.log(data);
+  
   return (
     <div>
       <h2>Quiz of {data.name}</h2>
       <div>
         {data.questions.map((ques) => (
-          <QuizQuestions key={ques.id} ques={ques}></QuizQuestions>
+          <QuizQuestions
+            key={ques.id}
+            ques={ques}
+          ></QuizQuestions>
         ))}
       </div>
     </div>
--- src/components/QuizQuestions/QuizQuestions.js
+++ src/components/QuizQuestions/QuizQuestions.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const QuizQuestions = ({ ques }) => {
-  const { question, options, id } = ques;
+  const { question, options, id, correctAnswer } = ques;
+  const [answer, setAnswer] = useState();
+
+  const getCorrectAnswer = (e) => {
+    setAnswer(e.target.value);
+  };
+  let message;
+  if (answer === correctAnswer) {
+    message = <p>Correct Answer</p>;
+  }
+
   return (
     <div>
       {<h3>{question}</h3>}
       {options.map((result) => (
         <div>
-          <input type="radio" name={id}></input>
+          <input
+            type="radio"
+            key={result.id}
+            name={id}
+            value={result}
+            onClick={getCorrectAnswer}
+          ></input>
           <label>{result}</label>
         </div>
       ))}
+      {message}
+      {/* <p>{answer}</p> */}
     </div>
   );
 };
